22384cb594cd5e7b2c6e401c037083d2b763d98b
KrasnovDaniil1 <kd69931@gmail.com>
2023-12-18 10:07:57 +0200

    add

--- assets/BlogID-4b2f4b31.js
+++
@@ -1,1 +0,0 @@
-import{_ as o,o as e,c as s}from"./index-9b8f00e7.js";const t={setup(){}},c={class:"blog"};function n(r,a,_,p,l,f){return e(),s("section",c)}const d=o(t,[["render",n]]);export{d as default};
